69013aff3b378b8b80196e30f401e994448e9aa8
Olivier Tilloy <olivier@tilloy.net>
2009-11-25 09:51:37 +0100

    Case insensitive matching of the type.

--- src/pyexiv2/xmp.py
+++ src/pyexiv2/xmp.py
@@ -118,12 +118,12 @@ class XmpTag(object):
         self._raw_value = value
         if self.type.startswith(('seq', 'bag', 'alt')):
             type = self.type[4:]
-            if type.startswith('closed Choice of'):
+            if type.lower().startswith('closed choice of'):
                 type = type[17:]
             self._value = map(lambda x: self._convert_to_python(x, type), value)
         elif self.type == 'Lang Alt':
             self._value = value
-        elif self.type.startswith('closed Choice of'):
+        elif self.type.lower().startswith('closed choice of'):
             self._value = self._convert_to_python(value, self.type[17:])
         else:
             self._value = self._convert_to_python(value, self.type)
