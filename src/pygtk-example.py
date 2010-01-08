907abb8e1954a33139f3af233b9ab355d500fa01
Olivier Tilloy <olivier@tilloy.net>
2010-01-08 09:24:09 +0100

    Adapted the pygtk thumbnail example.

--- src/pygtk-example.py
+++ src/pygtk-example.py
@@ -21,51 +21,57 @@
 # along with pyexiv2; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, 5th Floor, Boston, MA 02110-1301 USA.
 #
-#
-# File:      pyqt-example.py
-# Author(s): Olivier Tilloy <olivier@tilloy.net>
+# Author: Olivier Tilloy <olivier@tilloy.net>
 #
 # ******************************************************************************
 
 import sys
 import gtk
-import pyexiv2
+from pyexiv2 import ImageMetadata
+
 
 if __name__ == '__main__':
-	"""
-	Example of how to combine pygtk and pyexiv2 to display thumbnail data.
+    """
+    Example of how to combine pygtk and pyexiv2 to display thumbnail data.
 
-	Minimalistic example of how to load and display with pygtk the thumbnail
-	data extracted from an image using the method Image.getThumbnailData().
-	The path to the image file from which the thumbnail data should be extracted
-	should be passed as the only argument of the script.
+    Minimalistic example of how to load and display with pygtk the thumbnail
+    data extracted from an image.
+    The path to the image file from which the thumbnail data should be extracted
+    should be passed as the only argument of the script.
 
-	It is of course assumed that you have pygtk installed.
-	"""
-	if (len(sys.argv) != 2):
-		print 'Usage: ' + sys.argv[0] + ' path/to/picture/file/containing/jpeg/thumbnail'
-		sys.exit(1)
-	else:
-		app = gtk.Window(gtk.WINDOW_TOPLEVEL)
+    It is of course assumed that you have pygtk installed.
+    """
+    if (len(sys.argv) != 2):
+        print 'Usage: ' + sys.argv[0] + ' path/to/picture/file/containing/jpeg/thumbnail'
+        sys.exit(1)
 
-		# Load the image, read the metadata and extract the thumbnail data
-		image = pyexiv2.Image(sys.argv[1])
-		image.readMetadata()
-		ttype, tdata = image.getThumbnailData()
+    app = gtk.Window(gtk.WINDOW_TOPLEVEL)
 
-		# Create a GTK pixbuf loader to read the thumbnail data
-		pbloader = gtk.gdk.PixbufLoader()
-		pbloader.write(tdata)
-		# Get the resulting pixbuf and build an image to be displayed
-		pixbuf = pbloader.get_pixbuf()
-		pbloader.close()
-		imgwidget = gtk.Image()
-		imgwidget.set_from_pixbuf(pixbuf)
+    # Load the image, read the metadata and extract the thumbnail data
+    metadata = ImageMetadata(sys.argv[1])
+    metadata.read()
+    previews = metadata.previews
+    if not previews:
+        print "This image doesn't contain any thumbnail."
+        sys.exit(1)
 
-		# Show the application's main window
-		# Note: closing the window will not terminate the application as no
-		# appropriate signal has been defined.
-		app.add(imgwidget)
-		imgwidget.show()
-		app.show()
-		gtk.main()
+    # Get the largest preview available
+    preview = previews[-1]
+
+    # Create a pixbuf loader to read the thumbnail data
+    pbloader = gtk.gdk.PixbufLoader()
+    pbloader.write(preview.data)
+    # Get the resulting pixbuf and build an image to be displayed
+    pixbuf = pbloader.get_pixbuf()
+    pbloader.close()
+    imgwidget = gtk.Image()
+    imgwidget.set_from_pixbuf(pixbuf)
+
+    # Show the application's main window
+    # Note: closing the window will not terminate the application as no
+    # appropriate signal has been defined.
+    app.add(imgwidget)
+    imgwidget.show()
+    app.show()
+    sys.exit(gtk.main())
+
