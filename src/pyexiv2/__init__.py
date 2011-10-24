b62b89124e5867af9c3e7dc14b27438cb187d95f
Olivier Tilloy <olivier@tilloy.net>
2011-10-24 19:07:31 +0200

    Bumped version number to 0.3.2.

--- src/pyexiv2/__init__.py
+++ src/pyexiv2/__init__.py
@@ -75,7 +75,7 @@ def _make_version(version_info):
 
 
 #: A tuple containing the three components of the version number: major, minor, micro.
-version_info = (0, 3, 1)
+version_info = (0, 3, 2)
 
 #: The version of the module as a string (major.minor.micro).
 __version__ = _make_version(version_info)
